f9818cc6fb62af0edbe1ef3c7a71e08e81cf7b19
Igor Desktop <igfigueiredo@outlook.com>
2021-08-03 15:14:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+
+import Home from './Pages/Home';
+import Rooms from './Pages/Rooms';
+import SingleRoom from './Pages/SingleRoom';
+import Error from './Pages/Error';
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Route exact path="/" component={Home} />
+      <Route path="/rooms" component={Rooms} />
+      <Route path="/rooms/:room" component={SingleRoom} />
+      <Route path="/error" component={Error} />
     </div>
   );
 }
