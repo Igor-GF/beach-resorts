1ad822f4eb7c0bd434798988c741bfaef126a573
Igor Desktop <igfigueiredo@outlook.com>
2021-08-04 07:11:46 +0200

    support file to the tutorial

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from './Pages/Home';
 import Rooms from './Pages/Rooms';
 import SingleRoom from './Pages/SingleRoom';
 import Error from './Pages/Error';
+import Navbar from './Components/Navbar/Navbar';
 
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
+      <Navbar /> 
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
