5bae978b637d34f109eab5a8a05c128d939a2892
Igor Desktop <igfigueiredo@outlook.com>
2021-09-07 10:57:47 +0200

    featuredRooms component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero';
 import Banner from '../Components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../Components/Services';
+import FeaturedRooms from '../Components/FeaturedRooms';
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
       </Hero>
 
       <Services/>
+      <FeaturedRooms />
     </>
   )
 }
